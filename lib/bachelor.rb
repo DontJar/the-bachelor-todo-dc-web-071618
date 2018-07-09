deaf817b1aaa3a36f17ccdbcb8964907aedaa76c
Jon Schulman <schulman99@gmail.com>
2018-07-09 13:32:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,6 @@
 require "pry"
 def get_first_name_of_season_winner(data, season)
+winner = nil
   data[season].each do |full_contestant_data_array|
     full_contestant_data_array.each do |facts|
       facts.each do |stat, fact|

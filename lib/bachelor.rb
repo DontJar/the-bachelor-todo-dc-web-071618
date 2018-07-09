ad84255d2e61ef2e50c85a7fa8b495499fc8b884
Jon Schulman <schulman99@gmail.com>
2018-07-09 13:56:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,7 +3,7 @@ def get_first_name_of_season_winner(data, season)
 winner = nil
   data[season].each do |full_contestant_data_array|
     full_contestant_data_array.each do |facts|
-      facts.each do |stat|
+      facts.each do |stat, fact|
         if facts[1] == "Winner"
           winner = full_contestant_data_array["name"]
         end

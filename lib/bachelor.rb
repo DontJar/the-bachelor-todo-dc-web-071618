699d1e6ec7606bafbd3866f362bbe262f545f425
Jon Schulman <schulman99@gmail.com>
2018-07-09 14:09:33 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,7 +16,6 @@ contestant = nil
 data.map do |season, every_contestant_array|
   every_contestant_array.map do |full_contestant_data_array|
     full_contestant_data_array.map do |facts|
-      # binding.pry
       if facts[1] == occupation
         contestant = full_contestant_data_array["name"]
       end
